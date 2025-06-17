8bccd31601b02d95940f9bf563b224ac1b8851ac
Victor Garcia Zarco <victor.gzarco@gmail.com>
2025-06-17 16:29:26 +0200

    feat: add option to support multiple entries

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const runLinterOnRepo = require(path.join(__dirname, './lib/runLinterOnRepo'))
 
 async function main() {
   const rules = loadRules(path.join(__dirname,'./rules.js'))
-  const results = await runLinterOnRepo('force-app', rules)
+  const results = await runLinterOnRepo(['force-app'], rules)
 
 
   //Print results
   results.forEach(r => {
 	const icon = r.priority === 1 ? '🔴' : r.priority === 2 ? '🟡' : 'ℹ️'
-    console.log(`${icon} ${r.filePath}:${r.line} ▶️ [${r.rule}] ${r.message}`)
-    
+    //console.log(`${icon} ${r.filePath}:${r.line} ▶️ [${r.rule}] ${r.message}`)
+    console.log(`${r.filePath};${r.line};${r.rule};${r.message}`)
   })
 }
 
